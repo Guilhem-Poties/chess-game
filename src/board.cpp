2b089c1a723fbd1c0f61f43bf34ff78c0a7749c2
Guilhem <gpoties@gmail.com>
2025-02-04 09:37:52 +0100

    déclaration des fonctions de vérification de case pour les déplacements des pieces

--- src/board.cpp
+++ src/board.cpp
@@ -8,21 +8,21 @@ std::string get_case_written_coordonates(std::pair<int, int> coordonates)
 Board::Board()
 {
     // Generate the empty board
-    std::vector<Piece> pieces{};
+    std::vector<std::optional<Piece>> board{};
 
     // Pieces placement
     for (int i = 0; i < 8; i++)
     {
-        pieces.push_back(Piece((Piece_type)pieces_alignement[i], Color::black, 1, i));
-        pieces.push_back(Piece(Piece_type::Pawn, Color::black, 1, i));
-        pieces.push_back(Piece(Piece_type::Pawn, Color::black, 6, i));
-        pieces.push_back(Piece((Piece_type)pieces_alignement[i], Color::black, 7, i));
+        board.push_back(Piece((Piece_type)pieces_alignement[i], Color::black, 1, i));
+        board.push_back(Piece(Piece_type::Pawn, Color::black, 1, i));
+        board.push_back(Piece(Piece_type::Pawn, Color::black, 6, i));
+        board.push_back(Piece((Piece_type)pieces_alignement[i], Color::black, 7, i));
     };
 
-    this->pieces = pieces;
+    this->_board = board;
 }
 
-Piece Board::at(int pos_x, int pos_y)
+std::optional<Piece> Board::at(int pos_x, int pos_y)
 {
     //     auto const same_coordonates = [&](Piece& piece) {
     //     return piece.get_coordonates().first == pos_x && piece.get_coordonates().second == pos_y;
--- src/board.hpp
+++ src/board.hpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <optional>
 #include <string>
 #include <utility>
 #include <vector>
@@ -13,14 +14,20 @@ const std::vector<std::string> coordonates_letter{"A", "B", "C", "D", "E", "F", 
 
 class Board {
 private:
-    std::vector<Piece> pieces{};
+    std::vector<std::optional<Piece>> _board{};
 
 public:
     Board();
 
-    Piece at(int pos_x, int pos_y);
+    // Piece at(int pos_x, int pos_y);
+
+    std::optional<Piece> at(int x, int y);
 
     int n_piece();
+
+    bool is_in_board(int x, int y);
+    bool is_empty(int x, int y);
+    bool is_ennemy(int x, int y);
 };
 
 std::string get_case_written_coordonates(std::pair<int, int> coordonates);
