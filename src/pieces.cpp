4c61ba63344e30bc3cfb5f0015b43059d95eaa04
GuilhemRA <guilhem.rebejac@edu.esiee.fr>
2025-02-11 10:26:50 +0100

    Changement moves pieces

--- src/pieces.cpp
+++ src/pieces.cpp
@@ -1,41 +1,194 @@
 #include "pieces.hpp"
+#include "board.hpp"
 
-Piece::Piece(Piece_type piece_type, Color piece_color, int piece_pos_x, int piece_pos_y)
+// Piece::Piece(Piece_type piece_type, Color piece_color, int piece_pos_x, int piece_pos_y)
+// {
+//     this->type  = piece_type;
+//     this->color = piece_color;
+//     this->pos_x = piece_pos_x;
+//     this->pos_y = piece_pos_y;
+//     this->value = piece_value_by_type[(int)piece_type];
+// };
+
+// void Piece::move(int pos_x, int pos_y)
+// {
+//     this->pos_x = pos_x;
+//     this->pos_y = pos_y;
+// };
+
+// void Piece::remove()
+// {
+//     delete this;
+// };
+
+// int Piece::take(Piece taken_piece)
+// {
+//     int taken_value = taken_piece.value;
+
+//     this->move(taken_piece.pos_x, taken_piece.pos_y);
+
+//     taken_piece.remove();
+//     return taken_value;
+// };
+
+// std::pair<int, int> Piece::get_coordonates()
+// {
+//     return std::make_pair(this->pos_x, this->pos_y);
+// }
+
+// std::vector<std::pair<int, int>> Piece::get_moves()
+// {
+//     return {};
+// }
+
+// King
+std::vector<std::pair<int, int>> King::get_all_possible_moves(Board const& board, int x, int y)
 {
-    this->type  = piece_type;
-    this->color = piece_color;
-    this->pos_x = piece_pos_x;
-    this->pos_y = piece_pos_y;
-    this->value = piece_value_by_type[(int)piece_type];
-};
+    std::vector<std::pair<int, int>> all_moves{
+        {1, 1},
+        {0, 1},
+        {-1, 1},
+        {-1, 0},
+        {-1, -1},
+        {0, -1},
+        {1, -1}
+    };
 
-void Piece::move(int pos_x, int pos_y)
-{
-    this->pos_x = pos_x;
-    this->pos_y = pos_y;
-};
+    std::vector<std::pair<int, int>> current_possible_moves{};
 
-void Piece::remove()
-{
-    delete this;
-};
-
-int Piece::take(Piece taken_piece)
-{
-    int taken_value = taken_piece.value;
-
-    this->move(taken_piece.pos_x, taken_piece.pos_y);
-
-    taken_piece.remove();
-    return taken_value;
-};
-
-std::pair<int, int> Piece::get_coordonates()
-{
-    return std::make_pair(this->pos_x, this->pos_y);
+    for (std::pair<int, int> pair : all_moves)
+    {
+        int X = x + pair.first;
+        int Y = y + pair.second;
+        if (board.is_in_board(X, Y) && board.tile_state(X, Y, get_color()) != Tile_State::ally)
+        {
+            current_possible_moves.emplace_back(X, Y);
+        }
+    }
 }
 
-std::vector<std::pair<int, int>> Piece::get_moves()
+// Queen
+std::vector<std::pair<int, int>> Queen::get_all_possible_moves(Board const& board, int x, int y)
 {
-    return {};
+    std::vector<std::pair<int, int>> all_moves{
+        {1, 1},
+        {0, 1},
+        {-1, 1},
+        {-1, 0},
+        {-1, -1},
+        {0, -1},
+        {1, -1},
+        {1, 0}
+    };
+
+    std::vector<std::pair<int, int>> current_possible_moves{};
+
+    for (std::pair<int, int> pair : all_moves)
+    {
+        int X = x + pair.first;
+        int Y = y + pair.second;
+        while (board.is_in_board(X, Y))
+        {
+            if (board.is_in_board(X, Y) && board.tile_state(X, Y, get_color()) != Tile_State::ally)
+            {
+                current_possible_moves.emplace_back(X, Y);
+                X += pair.first;
+                Y += pair.second;
+            }
+        }
+    }
+}
+
+// Bishop
+std::vector<std::pair<int, int>> Bishop::get_all_possible_moves(Board const& board, int x, int y)
+{
+    std::vector<std::pair<int, int>> all_moves{
+        {1, 1},
+        {-1, 1},
+        {-1, -1},
+        {1, -1}
+    };
+
+    std::vector<std::pair<int, int>> current_possible_moves{};
+
+    for (std::pair<int, int> pair : all_moves)
+    {
+        int X = x + pair.first;
+        int Y = y + pair.second;
+        if (board.is_in_board(X, Y) && board.tile_state(X, Y, get_color()) != Tile_State::ally)
+        {
+            current_possible_moves.emplace_back(X, Y);
+        }
+    }
+}
+
+// Knight
+std::vector<std::pair<int, int>> Knight::get_all_possible_moves(Board const& board, int x, int y)
+{
+    std::vector<std::pair<int, int>> all_moves{
+        {2, 1},
+        {2, -1},
+        {1, 2},
+        {-1, 2},
+        {-2, 1},
+        {-2, -1},
+        {1, -2},
+        {-1, -2},
+    };
+
+    std::vector<std::pair<int, int>> current_possible_moves{};
+
+    for (std::pair<int, int> pair : all_moves)
+    {
+        int X = x + pair.first;
+        int Y = y + pair.second;
+        if (board.is_in_board(X, Y) && board.tile_state(X, Y, get_color()) != Tile_State::ally)
+        {
+            current_possible_moves.emplace_back(X, Y);
+        }
+    }
+}
+
+// Tower
+std::vector<std::pair<int, int>> Tower::get_all_possible_moves(Board const& board, int x, int y)
+{
+    std::vector<std::pair<int, int>> all_moves{
+        {0, 1},
+        {-1, 0},
+        {0, -1},
+        {1, 0}
+    };
+
+    std::vector<std::pair<int, int>> current_possible_moves{};
+
+    for (std::pair<int, int> pair : all_moves)
+    {
+        int X = x + pair.first;
+        int Y = y + pair.second;
+        if (board.is_in_board(X, Y) && board.tile_state(X, Y, get_color()) != Tile_State::ally)
+        {
+            current_possible_moves.emplace_back(X, Y);
+        }
+    }
+}
+
+// Pawn
+std::vector<std::pair<int, int>> Pawn::get_all_possible_moves(Board const& board, int x, int y)
+{
+    std::vector<std::pair<int, int>> all_moves{
+        {0, 1},
+        {0, 2}
+    };
+
+    std::vector<std::pair<int, int>> current_possible_moves{};
+
+    for (std::pair<int, int> pair : all_moves)
+    {
+        int X = x + pair.first;
+        int Y = y + pair.second;
+        if (board.is_in_board(X, Y) && board.tile_state(X, Y, get_color()) != Tile_State::ally)
+        {
+            current_possible_moves.emplace_back(X, Y);
+        }
+    }
 }
--- src/pieces.hpp
+++ src/pieces.hpp
@@ -1,13 +1,16 @@
+#pragma once
 #include <cstddef>
 #include <string>
 #include <utility>
 #include <vector>
 
+class Board;
+
 #ifndef PIECES_ATTRIBUTES
-const std::vector<int>         piece_value_by_type{9, 5, 3, 3, 1, 0};
-const std::vector<int>         piece_n_move_directions_by_type{8, 4, 4, 8, 3, 8};
-const std::vector<bool>        piece_inf_range_by_type{true, true, true, false, false, false};
-const std::vector<std::string> piece_written_name_by_type{"Queen", "Tower", "Bishop", "Knight", "Pawn", "King"};
+// const std::vector<int>         piece_value_by_type{9, 5, 3, 3, 1, 0};
+// const std::vector<int>         piece_n_move_directions_by_type{8, 4, 4, 8, 3, 8};
+// const std::vector<bool>        piece_inf_range_by_type{true, true, true, false, false, false};
+// const std::vector<std::string> piece_written_name_by_type{"Queen", "Tower", "Bishop", "Knight", "Pawn", "King"};
 
 // Direction operators used to create the mouvement operators
 // int front(int x)
@@ -90,159 +93,6 @@ const std::vector<std::string> piece_written_name_by_type{"Queen", "Tower", "Bis
 //         [](int x, int y) { return std::make_pair(x++, y--); },
 //     },
 // };
-class piece {
-public:
-    virtual std::vector<std::pair<int, int>> get_all_possible_moves() = 0;
-
-private:
-};
-
-// enum class Direction{
-//     Left,Right,
-
-// }
-
-class King : public piece {
-    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board) override
-    {
-        // if(board.is_empty(int x, int y))
-        return {
-            {1, 1},
-            {0, 1},
-            {-1, 1},
-            {-1, 0},
-            {-1, -1},
-            {0, -1},
-            {1, -1}
-        };
-    }
-};
-
-class Queen : public piece {
-    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) override
-    {
-        {
-            [](int x, int y) { return std::make_pair(x++, y); },
-            [](int x, int y) { return std::make_pair(x--, y); },
-            [](int x, int y) { return std::make_pair(x, y++); },
-            [](int x, int y) { return std::make_pair(x, y--); },
-            [](int x, int y) { return std::make_pair(x++, y++); },
-            [](int x, int y) { return std::make_pair(x--, y++); },
-            [](int x, int y) { return std::make_pair(x--, y--); },
-            [](int x, int y) { return std::make_pair(x++, y--); },
-        }
-
-        std::vector<std::pair<int, int>>
-            deplacement = {
-                {1, 1},
-                {0, 1},
-                {-1, 1},
-                {-1, 0},
-                {-1, -1},
-                {0, -1},
-                {1, -1},
-                {1, 0}
-            };
-        std::vector<std::pair<int, int>> deplacement_possible{};
-
-        for (std::pair<int, int> p : deplacement)
-        {
-            int X = x + p.first;
-            int Y = y + p.second;
-            while (board.is_in_board(X, Y))
-            {
-                if (board.is_empty(X, Y))
-                {
-                    if (!board.is_ennemy)
-                    {
-                        deplacement_possible.emplace_back(X, Y);
-                        X += p.first;
-                        Y += p.second;
-                    }
-                }
-            }
-        }
-        // if(board.is_empty(int x, int y))
-        return {
-            {1, 1},
-            {0, 1},
-            {-1, 1},
-            {-1, 0},
-            {-1, -1},
-            {0, -1},
-            {1, -1},
-            {1, 0}
-        };
-    }
-};
-
-class Bishop : public piece {
-    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board) override
-    {
-        // if(board.is_empty(int x, int y))
-        return {
-            {1, 1},
-            {-1, 1},
-            {-1, -1},
-            {1, -1}
-        };
-    }
-};
-
-class Knight : public piece {
-    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board) override
-    {
-        // if(board.is_empty(int x, int y))
-        return {
-            {2, 1},
-            {2, -1},
-            {1, 2},
-            {-1, 2},
-            {-2, 1},
-            {-2, -1},
-            {1, -2},
-            {-1, -2},
-        };
-    }
-};
-
-class Tower : public piece {
-    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board) override
-    {
-        // if(board.is_empty(int x, int y))
-        return {
-            {0, 1},
-            {-1, 0},
-            {0, -1},
-            {1, 0}
-        };
-    }
-};
-
-class Pawn : public piece {
-    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board) override
-    {
-        // if(board.is_empty(int x, int y))
-        return {
-            {0, 1},
-            {0, 2}
-        };
-    }
-};
-
-const std::vector<int>
-    pieces_alignement{1, 3, 2, 0, 5, 2, 3, 1};
-
-#endif
-
-enum class Piece_type : int {
-    Queen,
-    Tower,
-    Bishop,
-    Knight,
-    Pawn,
-    King
-};
 
 enum class Color : int {
     black,
@@ -250,29 +100,82 @@ enum class Color : int {
 };
 
 class Piece {
-private:
-    int pos_x{};
-    int pos_y{};
-
-    int value;
-
-    bool as_moved{false};
-
 public:
-    Piece_type type{};
-    Color      color{};
+    virtual std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) = 0;
 
-    Piece(Piece_type piece_type, Color color, int pos_x, int pos_y);
+    Color get_color() const { return color; }
 
-    // The piece move
-    void move(int pos_x, int pos_y);
-    // The piece is taken and removed from the board
-    void remove();
-    // Take another piece, return the value of the taken piece
-    int take(Piece taken_piece);
-
-    std::pair<int, int> get_coordonates();
-
-    // Get the possible moves of the piece
-    std::vector<std::pair<int, int>> get_moves();
+private:
+    Color color;
 };
+
+// enum class Direction{
+//     Left,Right,
+
+// }
+
+class King : public Piece {
+    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) override;
+};
+
+class Queen : public Piece {
+    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) override;
+};
+
+class Bishop : public Piece {
+    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) override;
+};
+
+class Knight : public Piece {
+    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) override;
+};
+
+class Tower : public Piece {
+    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) override;
+};
+
+class Pawn : public Piece {
+    std::vector<std::pair<int, int>> get_all_possible_moves(Board const& board, int x, int y) override;
+};
+
+const std::vector<int>
+    pieces_alignement{1, 3, 2, 0, 5, 2, 3, 1};
+
+#endif
+
+// enum class Piece_type : int {
+//     Queen,
+//     Tower,
+//     Bishop,
+//     Knight,
+//     Pawn,
+//     King
+// };
+
+// class Piece {
+// private:
+//     int pos_x{};
+//     int pos_y{};
+
+//     int value;
+
+//     bool as_moved{false};
+
+// public:
+//     Piece_type type{};
+//     Color      color{};
+
+//     Piece(Piece_type piece_type, Color color, int pos_x, int pos_y);
+
+//     // The piece move
+//     void move(int pos_x, int pos_y);
+//     // The piece is taken and removed from the board
+//     void remove();
+//     // Take another piece, return the value of the taken piece
+//     int take(Piece taken_piece);
+
+//     std::pair<int, int> get_coordonates();
+
+//     // Get the possible moves of the piece
+//     std::vector<std::pair<int, int>> get_moves();
+// };
